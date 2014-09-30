2ebb4a634a83213de53534934f097f8fed286f43
Sudheesh Singanamalla <sudheesh1995@outlook.com>
2014-09-30 17:49:33 +0530

    Create databases.py

---
+++ main/main/databases.py
@@ -0,0 +1,10 @@
+DATABASES = {
+	'default':{
+	'ENGINE':'django.db.backends.mysql',
+	'NAME':'MozEvangelist',
+	'USER':'',
+	'PASSWORD':'', # Enter your password here
+	'HOST':'127.0.0.1',
+	'PORT':'3306'
+	}
+}
